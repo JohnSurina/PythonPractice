e999e46b1d1957abac88a4e3e338d1e40f56b89e
John Surina <bigboss@Johns-MBP-2015.verizon.net>
2019-11-24 20:35:24 -0500

    check in tic tac gui

--- TicTacToe/TicTacGUI.py
+++ TicTacToe/TicTacGUI.py
@@ -1,12 +1,37 @@
 import TicTacToeBoard as TTT
 from tkinter import *
+from tkinter import ttk
 
+## Define control methods ##
+
+def onReset():
+    print("Reset button invoked.")
+
+############################
+
+## Define Primary GUI container
 root = Tk(className="TicTacToe")
-mFrame = Frame(root).grid()
 
-button = Button(mFrame, command = lambda:print("hello!")).grid()
-button2 = Button(mFrame, command = lambda:print("goodbye!")).grid()
+## Define Frames
+mainFrame = ttk.Frame(root)
+mainFrame.grid(sticky="NEWS")
+for i in range(0,2): mainFrame.columnconfigure(i, weight=1);
 
-label = Label(root).grid
+gameFrame = ttk.Frame(mainFrame,padding=10)
+gameFrame.grid(column=0, row=1, sticky="WE")
+
+controlFrame = ttk.Frame(mainFrame,padding=10)
+controlFrame.grid(column=1, row=1, sticky="WE")
+
+## Define GUI Variables and content options.
+whosTurnPossibilities = ['Player 1\'s turn','Player 2\'s turn']
+whosTurn = StringVar()
+
+## Define Widgets
+turnTracker = ttk.Label(mainFrame, textvariable=whosTurn)
+turnTracker.grid(column=0,row=0)
+
+resetButton = ttk.Button(controlFrame, text="Reset", command=onReset)
+resetButton.grid(column=1, row=0)
 
 root.mainloop()
